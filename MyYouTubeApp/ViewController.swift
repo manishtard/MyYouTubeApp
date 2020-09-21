00067c3483295765b989abe0f70d441b4f551ba9
Manish Tard <tardmanish@gmail.com>
2020-09-21 18:04:51 +0530

    initial XCode Project creation

---
+++ MyYouTubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyYouTubeApp
+//
+//  Created by Manish Tard on 21/09/20.
+//  Copyright © 2020 Manish Tard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
